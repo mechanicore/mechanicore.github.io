44da4f3080317758932c7f9bbc296426e8608322
mechanicore <mechanicoreframe@gmail.com>
2019-09-11 23:29:58 +0800

    Testing build only from dev branch.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,12 @@ set -ev
 
 if [ "${GITHUB_API_TOKEN}" ]; then
   npm run lint
-fi
 
-# Build and deploy only from `dev` branch
-if [ "${TRAVIS_BRANCH}" = 'dev' ]; then
-  npm run build
-  cp README.md dist/
-  cd dist
-  ls -l -a
+  # Build only from `dev` branch
+  if [ "${TRAVIS_BRANCH}" = 'dev' ]; then
+    npm run build
+    cp README.md dist/
+    cd dist
+    ls -l -a
+  fi
 fi
